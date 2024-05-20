202043c6ea719ac3ac189a142c588759f5cfd004
10210815 <10210815@B206PC24>
2024-05-20 10:50:47 +0300

    Добавьте файлы проекта.

---
+++ TechStore/DbContextTech.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechStore
+{
+    internal class DbContextTech
+    {
+        public static technicalstoreEntities entity;
+    }
+}
---
+++ TechStore/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TechStore
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DbContextTech.entity = new technicalstoreEntities();
+            ListView1.ItemsSource = DbContextTech.entity.goods.ToList();
+            SortBy.ItemsSource = new string[] { "name", "price" };
+            sortProp.ItemsSource = Enum.GetNames(typeof(ListSortDirection));
+            ListView1.Items.SortDescriptions.Add(new SortDescription("name", ListSortDirection.Ascending));
+
+        }
+    }
+}
